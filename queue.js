5b671b79f596b332b0615f41d6f0e0681185fe3a
emonShimoul <emonshimoul@gmail.com>
2023-01-18 00:52:11 +0600

    work with fifo

--- queue.js
+++ queue.js
@@ -1,6 +1,27 @@
 // First In First Out => FIFO
 class Queue {
     constructor() {
-
+        this.queue = [];
+    }
+    enqueue(element) {
+        this.queue.push(element);
+    }
+    deque() {
+        this.queue.shift();
     }
 }
+
+const premiks = new Queue();
+premiks.enqueue('Ashiq');
+premiks.enqueue('Dewan');
+premiks.enqueue('Prem');
+premiks.enqueue('Sagor');
+premiks.enqueue('Raj');
+console.log(premiks.queue);
+premiks.deque();
+console.log(premiks.queue);
+premiks.deque();
+console.log(premiks.queue);
+premiks.deque();
+console.log(premiks.queue);
+
